129f2a1d712ca7a83d8f7a45562901504aaf82ce
DanielQuilcue <danielquilcue18@gmail.com>
2025-01-07 12:03:20 -0500

    fix style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import * as XLSX from "xlsx";
 import DefaultLayout from "@/layouts/default";
 import Cardcita from "@/components/Card/Cardcita";
 import { Button, Input } from "@nextui-org/react";
+import { CardcitaProps } from "../interface";
 import "../styles/style.css";
-
 export default function IndexPage() {
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<CardcitaProps[]>([]);
 
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -14,11 +14,15 @@ export default function IndexPage() {
       const reader = new FileReader();
       reader.onload = (evt) => {
         const binaryStr = evt.target?.result;
-        const workbook = XLSX.read(binaryStr, { type: "binary" });
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(sheet);
-        setData(jsonData);
+        try {
+          const workbook = XLSX.read(binaryStr, { type: "binary" });
+          const sheetName = workbook.SheetNames[0];
+          const sheet = workbook.Sheets[sheetName];
+          const jsonData: CardcitaProps[] = XLSX.utils.sheet_to_json(sheet);
+          setData(jsonData);
+        } catch (error) {
+          console.error("Error reading Excel file:", error);
+        }
       };
       reader.readAsBinaryString(file);
     }
@@ -29,29 +33,31 @@ export default function IndexPage() {
   };
 
   return (
-    <>
-      <DefaultLayout>
-        <section className="flex flex-col items-center justify-center gap-4 py-2">
-          <div className="inline-block max-w-lg text-center justify-center no-print">
-            <div className="justify-center mb-4">
-              {data.length > 0 ? (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-2">
+        <div className="inline-block max-w-lg text-center justify-center no-print">
+          <div className="justify-center mb-4">
+            {data.length > 0 ? (
+              <>
                 <Button color="primary" variant="ghost" onClick={handlePrint}>
                   Imprimir
                 </Button>
-              ) : (
-                <Input
-                  type="file"
-                  label="Excel"
-                  accept=".xlsx, .xls"
-                  onChange={handleFileUpload}
-                />
-              )}
-            </div>
+                <p>Total de escarapelas: {data.length}</p>
+              </>
+            ) : (
+              <Input
+                type="file"
+                label="Excel"
+                accept=".xlsx, .xls"
+                onChange={handleFileUpload}
+              />
+            )}
           </div>
-          <div className="grid justify-center mb-4 printable-content">
-            {data.map((item, index) => (
+        </div>
+        <div className="printable-content">
+          {data.map((item, index) => (
+            <div key={index} className="card-container">
               <Cardcita
-                key={index}
                 nombre={item.NOMBRE}
                 cc={item.CC}
                 cargo={item.CARGO}
@@ -59,10 +65,11 @@ export default function IndexPage() {
                 chaqueta={item.CHAQUETA}
                 camisa={item.CAMISA}
               />
-            ))}
-          </div>
-        </section>
-      </DefaultLayout>
-    </>
+              {index % 2 === 1 && <div className="page-break"></div>}
+            </div>
+          ))}
+        </div>
+      </section>
+    </DefaultLayout>
   );
 }
